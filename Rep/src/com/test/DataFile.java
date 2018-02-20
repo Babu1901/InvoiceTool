3949a102e27c12e2720b9050c2def74b50c52dde
training <training@CLBLAP706.BRILLIO.COM>
2018-02-19 23:07:31 -0800

    This is the commit from veeru

---
+++ Rep/src/com/test/DataFile.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class DataFile {
+int data[]={0,12,35};
+public void dis()
+{
+	System.out.println("hi veeru");
+}
+}
---
+++ Rep/src/com/test/MyMain.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class MyMain {
+public static void main(String[] args) {
+	DataFile d = new DataFile();
+	show();
+	d.dis();
+}
+public static void show()
+{
+	System.out.println("Happy sales");
+	System.out.println("Data injected");
+}
+}
